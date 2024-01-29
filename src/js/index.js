af77ab08fa45ddfc2518b003c8c323afac4085bf
WillianDantaas <88786113+WillianDantaas@users.noreply.github.com>
2024-01-29 17:01:28 -0300

    projeto dias

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const button = document.getElementsByClassName('hambmobile')
+const navmobile = document.getElementById('navmobile')
+
+button[0].addEventListener('click', () => {
+    navmobile.classList.toggle('hidden')
+})
+
+
+
